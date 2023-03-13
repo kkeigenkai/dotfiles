638d9d546226b2e3c69bb08a8efa90f4ceed4c47
Sergei Pshonnov <43892086+kkeigenkai@users.noreply.github.com>
2023-03-13 20:10:04 +0300

    Updated Doom Emacs config.

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,3 +1,4 @@
+;;; ~/.dotfiles/doom.d/config.el -*- lexical-binding: t; -*-
 (setq user-full-name "Sergei Pshonnov"
       user-mail-address "sergei@pshonnov.ru")
 
@@ -25,6 +26,7 @@
   (defvar +zen--old-writeroom-global-effects writeroom-global-effects)
   (setq writeroom-global-effects nil)
   (setq writeroom-maximize-window nil)
+  (setq writeroom-width 0.6)
 
   (add-hook! 'writeroom-mode-hook :append
     (defun +zen-enable-text-scaling-mode-h ()
@@ -40,6 +42,124 @@
 
 (setq org-directory "~/org/")
 (setq org-hide-emphasis-markers t)
+
+
+
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
+(use-package org-roam
+  :ensure t
+  :demand t
+  :init
+  (setq org-roam-v2-ack t)
+  :bind (
+         ("C-c n I" . #'org-roam-node-insert-immediate)
+         ("C-c n p" . #'my/org-roam-find-project)
+         ("C-c n b" . #'my/org-roam-capture-inbox)
+         ("C-c n t" . #'my/org-roam-capture-task))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :custom
+  (org-roam-directory "~/notes")
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("l" "programming language" plain
+      "* Characteristic:\n\n- Family: %?\n- Inspired by: \n\n* Reference:\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("p" "project" plain
+      (file "~/notes/.templates/ProjectTemplate.org")
+      :if-new (file "%<%Y%m%d%H%M%S>-${slug}.org")
+      :unnarrowed t)))
+  :config
+  (require 'org-roam-dailies)
+  (org-roam-db-autosync-mode))
+
+(defun my/org-roam-filter-by-tag (tag-name)
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  (mapcar #'org-roam-node-file
+          (seq-filter
+           (my/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun my/org-roam-refresh-agenda-list ()
+  (interactive)
+  (setq org-agenda-files (my/org-roam-list-notes-by-tag "Project")))
+
+(my/org-roam-refresh-agenda-list)
+
+(defun my/org-roam-project-finalize-hook ()
+  (remove-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-line-name)))))
+
+(defun my/org-roam-find-project ()
+  (interactive)
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "Project")
+   nil
+   :templates
+   '(("p" "project" plain
+      (file "~/notes/.templates/ProjectTemplate.org")
+      :if-new (file "%<%Y%m%d%H%M%S>-${slug}.org")
+      :unnarrowed t))))
+
+(setq org-roam-dailies-directory "daily/")
+
+(defun my/org-roam-capture-inbox ()
+  (interactive)
+  (org-roam-capture- :node (org-roam-node-create)
+                     :templates '(("i" "inbox" plain "* %?"
+                                   :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
+
+(defun my/org-roam-capture-task ()
+  (interactive)
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (org-roam-capture- :node (org-roam-node-read
+                            nil
+                            (my/org-roam-filter-by-tag "Project"))
+                     :templates '(("p" "project" plain "* TODO %?"
+                                   :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
+                                                          ("Tasks"))))))
+
+(defun my/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+        (org-roam-dailies-capture-templates
+          '(("t" "tasks" entry "%?"
+             :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        (org-after-refile-insert-hook #'save-buffer)
+        today-file
+        pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point)))
+
+    ;; Only refile if the target file is different than the current file
+    (unless (equal (file-truename today-file)
+                   (file-truename (buffer-file-name)))
+      (org-refile nil nil (list "Tasks" today-file nil pos)))))
+
+(add-to-list 'org-after-todo-state-change-hook
+             (lambda ()
+               (when (equal org-state "DONE")
+                 (my/org-roam-copy-todo-to-today))))
 
 (use-package! org-modern
   :after (org org-agenda org-roam)
@@ -74,20 +194,6 @@
         ("src" "»" "«")
         ("example" "»–" "–«")
         ("quote" "❝" "❞")))
-
-(setq org-roam-directory "~/notes")
-
-(defun org-roam-node-insert-immediate (arg &rest args)
-  (interactive "P")
-  (let ((args (cons arg args))
-        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
-                                                  '(:immediate-finish t)))))
-    (apply #'org-roam-node-insert args)))
-
-(use-package org-roam
-  :bind (
-         ("C-c n I" . org-roam-node-insert-immediate)
-         ))
 
 (use-package org-auto-tangle
   :defer t
